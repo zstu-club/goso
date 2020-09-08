4ba8bb0a82684a742db803ce02892bdd9a419821
huyukang <1628321264@qq.com>
2020-09-08 14:11:12 +0800

    [update] something

--- conn.go
+++ conn.go
@@ -1,7 +1,11 @@
 package goso
 
 import (
+	"encoding/json"
+	"errors"
 	"net"
+	"reflect"
+	"strconv"
 )
 
 type Conn struct {
@@ -16,6 +20,28 @@ func (c *Conn) SendBytes(data []byte) (int, error) {
 
 func (c *Conn) SendString(data string) (int, error) {
 	return c.Write([]byte(data))
+}
+
+func (c *Conn) Send(data interface{})(int, error){
+	var msg string
+	switch value := reflect.ValueOf(data); value.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		msg = strconv.FormatInt(value.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		msg = strconv.FormatUint(value.Uint(), 10)
+	case reflect.Map, reflect.Struct, reflect.Array, reflect.Slice:
+		marshal, err := json.Marshal(value)
+		if err != nil {
+			return 0, err
+		}
+		msg = string(marshal)
+	case reflect.Ptr:
+		return c.Send(value.Elem().Interface())
+	default:
+		return 0, errors.New("unsupported type")
+	}
+
+	return c.SendString(msg)
 }
 
 // 接收数据 todo 如何接收全部的数据而不是1024字节
